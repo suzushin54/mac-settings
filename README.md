8d9234680af1ab83f6275aebf04ff112b9175f45
suzushin54 <s.suzuki416@gmail.com>
2021-11-06 18:10:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+curl -O https://raw.githubusercontent.com/suzushin54/mac-settings/master/setting.sh
+chmod 755 ./setting.sh
+sh ./setting.sh
+```
---
+++ setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REP_PATH=~/rep//mac-settings
+
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ansible
+
+git clone https://github.com/suzushin54/mac-settings.git ${REP_PATH}
+cd ${REP_PATH}
+ansible-playbook -i inventory/localhost localhost.yml
